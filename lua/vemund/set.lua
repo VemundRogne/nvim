c0387804b5e4fb6fd684de71a3e202217c3453a1
Vemund Rogne <vemundrogne@gmail.com>
2023-01-22 15:30:50 +0100

    Only configure to powershell if sysname == 'Windows_NT'

--- lua/vemund/set.lua
+++ lua/vemund/set.lua
@@ -26,13 +26,15 @@ vim.opt.smartindent = true
 vim.opt.updatetime = 300
 
 -- Terminal configuration
-vim.cmd([[
-let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-set shellquote= shellxquote=
-]])
+if vim.loop.os_uname().sysname == 'Windows_NT' then
+    vim.cmd([[
+    let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+    let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+    let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    set shellquote= shellxquote=
+    ]])
+end
 
 -- Enable esc to go out of terminal mode
 function _G.set_terminal_keymaps()
