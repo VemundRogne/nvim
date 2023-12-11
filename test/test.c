fa2524536ef6e7c6e3ed77aba546b110b8b44a7d
= <vemund.rogne@autostoresystem.com>
2023-12-11 13:12:04 +0100

    Formatted test.c

--- test/test.c
+++ test/test.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main() {
-    FILE * fp;
+  FILE *fp;
 
-    fp = fopen("test\\input.txt", "r");
-    if (fp == NULL) {
-        exit(EXIT_FAILURE);
-    }
+  fp = fopen("test\\input.txt", "r");
+  if (fp == NULL) {
+    exit(EXIT_FAILURE);
+  }
 
-    char buffer[128];
+  char buffer[128];
 
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-        printf("Retrieved line of length %s:\n", buffer);
-    }
+  while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+    printf("Retrieved line of length %s:\n", buffer);
+  }
 
-    fclose(fp);
-    exit(0);
+  fclose(fp);
+  exit(0);
 }
