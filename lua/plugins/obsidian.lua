b503fa7cdc8d00899c27d2b69bcca4cb2f72a46e
Vemund <vemundrogne@gmail.com>
2024-12-23 17:49:27 +0100

    feat: update obsidian config

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,20 +1,20 @@
---print("Hostname =:" .. require("../utils.hostname").get_hostname())
-
+-- We want different workspaces for different computers
 local function workspaces_for_hostname()
 	local hostname = require("../utils.hostname").get_hostname()
 	local configs = {
+
+		-- My personal laptop:
 		["vemundyogalaptop"] = function()
 			return {
-				--workspaces = {
 				{
 					name = "wiki",
 					path = "/home/vemund/vaults/wiki/",
 				},
-				--},
 			}
 		end,
 	}
 
+	-- No point in serving a default... Obsidian gets upset if we pass something empty...
 	return configs[hostname] and configs[hostname]()
 end
 
@@ -26,6 +26,14 @@ return {
 	config = function()
 		require("obsidian").setup({
 			workspaces = workspaces_for_hostname(),
+		})
+
+		require("commander").add({
+			{
+				desc = "Obsidian quick switch",
+				cmd = "<CMD>ObsidianQuickSwitch<CR>",
+				keys = { "n", "<leader>wf", noremap },
+			},
 		})
 	end,
 }
